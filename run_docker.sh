148acd90b958df04e0c6f6e001cdcc2a99cbfa30
Moses Marquez <49002466+nullobsidian@users.noreply.github.com>
2021-09-02 19:34:07 +0000

    run docker

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build . -t udacity-microservices:latest
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker run --name flask-app -p 80:8080 udacity-microservices:latest
